c7708aa05fa6e7198bccca0fa3835775df1eedbe
Abdurahim Hudulov <136490387+abdurahim-H@users.noreply.github.com>
2024-10-21 10:07:09 +0200

    Changed main

---
+++ src/main.c
@@ -0,0 +1,74 @@
+#include "minishell.h"
+
+void prompt_init(t_mini *mini, int exit_code)
+{
+	char *prompt;
+
+	while (1)
+	{
+		prompt = readline("Minishell$ ");
+		if (!prompt)
+		{
+			printf("exit\n");
+			rl_clear_history();
+			ft_gc_destroy_all_contexts();
+			exit(EXIT_FAILURE);
+		}
+		add_history(prompt);
+		mini->exitcode = exit_code;
+		parse_init(mini, prompt);
+		exit_code = mini->exitcode;
+	}
+	rl_clear_history();
+	ft_gc_destroy_all_contexts();
+}
+
+void check_leaks(void)
+{
+	int ret = system("leaks minishell");
+	if (ret == -1)
+	{
+		perror("system");
+	}
+}
+
+char **duplicate_envp(char **envp)
+{
+    int     i;
+    int     count;
+    char    **new_envp;
+
+    count = 0;
+    while (envp[count])
+        count++;
+    new_envp = ft_gc_malloc(sizeof(char *) * (count + 1));
+    if (!new_envp)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    i = 0;
+    while (i < count)
+    {
+        new_envp[i] = ft_strdup(envp[i]);
+        i++;
+    }
+    new_envp[i] = NULL;
+    return (new_envp);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	t_mini  *mini;
+
+	(void)argc;
+	(void)argv;
+	mini = ft_gc_malloc(sizeof(t_mini));
+	mini->envp = duplicate_envp(envp);
+	mini->exitcode = 0;
+
+	setup_signal_handlers();
+	prompt_init(mini, mini->exitcode);
+
+	return (0);
+}
